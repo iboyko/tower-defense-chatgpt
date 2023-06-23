e0563d98be61ae23a34f469577beb12eb2f86591
Illya Graf-Boyko <illya.grafboyko@ebcont.com>
2023-06-23 13:33:14 +0200

    tower can shoot

--- game3.py
+++ game3.py
@@ -40,6 +40,7 @@ class Enemy:
         self.width = 20
         self.height = 20
         self.vel = 2
+        self.health = 10
 
     def move(self):
         self.x += self.vel
@@ -50,6 +51,21 @@ class Enemy:
     def reached_end(self):
         return self.x >= WIDTH
 
+# Bullet class
+class Bullet:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.radius = 5
+        self.color = WHITE
+        self.vel = 5
+
+    def move(self):
+        self.x += self.vel
+
+    def draw(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
 # Tower class
 class Tower:
     def __init__(self, x, y):
@@ -59,22 +75,34 @@ class Tower:
         self.height = 50
         self.range = 150
         self.damage = 1
+        self.cooldown = 60  # 1 second cooldown at 60 frames per second
+        self.can_shoot = True
 
     def draw(self):
         pygame.draw.rect(win, GREEN, (self.x, self.y, self.width, self.height))
 
     def shoot(self, enemy):
-        if self.is_within_range(enemy):
-            enemy.health -= self.damage
+        if self.can_shoot and self.is_within_range(enemy):
+            bullet = Bullet(self.x + self.width // 2, self.y + self.height // 2)
+            bullets.append(bullet)
+            self.can_shoot = False
 
     def is_within_range(self, enemy):
         distance = ((self.x - enemy.x) ** 2 + (self.y - enemy.y) ** 2) ** 0.5
         return distance <= self.range
 
+    def update_cooldown(self):
+        if not self.can_shoot:
+            self.cooldown -= 1
+            if self.cooldown <= 0:
+                self.can_shoot = True
+                self.cooldown = 60  # Reset the cooldown
+
 # Create objects
 player = Player()
 enemies = [Enemy()]
 towers = []
+bullets = []
 
 # Game loop
 running = True
@@ -97,6 +125,19 @@ while running:
     for enemy in enemies:
         enemy.move()
 
+    # Update towers
+    for tower in towers:
+        tower.update_cooldown()
+        for enemy in enemies:
+            tower.shoot(enemy)
+
+    # Move bullets
+    for bullet in bullets:
+        bullet.move()
+
+    # Remove bullets that are off-screen
+    bullets = [bullet for bullet in bullets if bullet.x <= WIDTH]
+
     # Draw the game window
     win.fill((0, 0, 0))  # Clear the screen
 
@@ -107,6 +148,10 @@ while running:
     # Draw towers
     for tower in towers:
         tower.draw()
+
+    # Draw bullets
+    for bullet in bullets:
+        bullet.draw()
 
     # Draw player stats
     player.draw()
